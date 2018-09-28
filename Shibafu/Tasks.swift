4e8cc21f1ce44e2d0d2e772e5d426303df486134
yuu-eguci <yuu.eguci@gmail.com>
2018-09-28 19:45:43 +0900

    develop Dropboxから基本データを作成

--- Shibafu/Tasks.swift
+++ Shibafu/Tasks.swift
@@ -16,10 +16,11 @@ class Tasks {
     static var modifiedDate:Date = Date()
     static var normals:[String] = []
     static var keeps:[String] = []
-    static var dones:[Date:[String]] = [:]
+    static var dones:[String:[String]] = [:]
     
     
-    static func downloadTasks() {
+    // DLしてnormals,keeps,donesを埋めて、テーブルを更新します。
+    static func downloadTasks(table:UITableView) {
         
         // Dropbox認証。
         guard let client = DropboxClientsManager.authorizedClient else {
@@ -35,9 +36,101 @@ class Tasks {
                 
                 // ファイルの内容です。
                 let originalText:String? = String(data:response.1, encoding:.utf8)
-                print(originalText)
+                
+                // originalText -> lines
+                let lines:[String] = self.convertTextToArray(text:originalText!)
+                
+                // normals作成。
+                self.normals = self.pickNormalTasks(lines: lines)
+                
+                // keeps作成。
+                self.keeps = self.pickKeepTasks(lines: lines)
+                
+                // dones作成。
+                self.dones = self.pickDoneTasks(lines: lines)
+                
+                table.reloadData()
             }
             
         }
+    }
+    
+    
+    // テキストをリストにします。
+    private static func convertTextToArray(text:String) -> [String] {
+        
+        // 配列にしつつ空行削除。
+        var lines:[String] = text.components(separatedBy: "\n").filter {$0 != ""}
+        var lines2:[String] = []
+        var i = 0
+        while lines.indices.contains(i) {
+            
+            // インデント行はタスク行に統合します。
+            var line:String = lines[i]
+            while lines.indices.contains(i+1) && lines[i+1].prefix(1)==" " {
+                i += 1
+                line += "\n" + lines[i]
+            }
+            lines2.append(line)
+            i += 1
+        }
+        return lines2
+    }
+    
+    
+    // リストからノーマルタスク取り出します。
+    private static func pickNormalTasks(lines:[String]) -> [String] {
+        
+        var ret:[String] = []
+        for line in lines {
+            
+            if Utils.isDoneDateRow(line: line) {
+                return ret
+            }
+            if Utils.isKeepTask(line: line) {
+                continue
+            }
+            ret.append(line)
+        }
+        return ret
+    }
+    
+    
+    // リストから継続タスク取り出します。
+    private static func pickKeepTasks(lines:[String]) -> [String] {
+        
+        var ret:[String] = []
+        for line in lines {
+            if Utils.isDoneDateRow(line: line) {
+                return ret
+            }
+            if Utils.isKeepTask(line: line) {
+                ret.append(line)
+            }
+        }
+        return ret
+    }
+    
+    
+    // リストからDoneタスク取り出します。
+    private static func pickDoneTasks(lines:[String]) -> [String:[String]] {
+        
+        var ret:[String:[String]] = [:]
+        var i = 0
+        while lines.indices.contains(i) {
+            
+            if !Utils.isDoneDateRow(line: lines[i]) {
+                i += 1
+                continue
+            }
+            let date:String = String(lines[i].suffix(10))
+            var tasks:[String] = []
+            while lines.indices.contains(i+1) && !Utils.isDoneDateRow(line: lines[i+1]) {
+                i += 1
+                tasks.append(lines[i])
+            }
+            ret[date] = tasks
+        }
+        return ret
     }
 }
