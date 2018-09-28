365d04500ec8ab15ef1065d9a53671698116d062
yuu-eguci <yuu.eguci@gmail.com>
2018-09-28 16:42:21 +0900

    Initial Commit

---
+++ Shibafu/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Shibafu
+//
+//  Created by Midori on 2018/09/28.
+//  Copyright © 2018年 Mate. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
