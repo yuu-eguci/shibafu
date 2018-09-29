a3d1e3782a6fec570c7a11ea30c30fe8fb894051
midori-mate <midori.mate@gmail.com>
2018-09-29 12:23:06 +0900

    develop 3ページ目にボタンを設置

---
+++ Shibafu/ThirdViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ThirdViewController.swift
+//  Shibafu
+//
+//  Created by Midori on 2018/09/29.
+//  Copyright © 2018年 Mate. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    
+    // ログインボタン押したとき。
+    @IBAction func loginButton(_ sender: Any) {
+    }
+    
+    
+    // ログアウトボタン押したとき。
+    @IBAction func logoutButton(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
