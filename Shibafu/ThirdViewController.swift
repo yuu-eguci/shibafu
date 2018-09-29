9e21a277685d412ba30152910a18387a253337cf
yuu-eguci <yuu.eguci@gmail.com>
2018-09-29 12:33:12 +0900

    develop 3ページ目にリンクとアンリンク実装

--- Shibafu/ThirdViewController.swift
+++ Shibafu/ThirdViewController.swift
@@ -7,17 +7,37 @@
 //
 
 import UIKit
+import SwiftyDropbox
 
 class ThirdViewController: UIViewController {
     
     
+    // ラベル。
+    @IBOutlet weak var label: UILabel!
+    
+    
     // ログインボタン押したとき。
     @IBAction func loginButton(_ sender: Any) {
+        
+        if let _ = DropboxClientsManager.authorizedClient {
+            label.text = "Already logined."
+        } else {
+            DropboxClientsManager.authorizeFromController(
+                .shared, controller: self, openURL: {(url:URL) -> Void in UIApplication.shared.open(url)})
+            label.text = "Logined!"
+        }
     }
     
     
     // ログアウトボタン押したとき。
     @IBAction func logoutButton(_ sender: Any) {
+        
+        if let _ = DropboxClientsManager.authorizedClient {
+            DropboxClientsManager.unlinkClients()
+            label.text = "Unlinked."
+        } else {
+            label.text = "Not logined."
+        }
     }
     
     
